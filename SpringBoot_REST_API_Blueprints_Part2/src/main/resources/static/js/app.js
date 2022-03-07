7723e29d34474557c6c7d397ee76dba05ccd77f5
unknown <juan.espinosa-t@labinfo.is.escuelaing.edu.co>
2022-03-07 10:18:57 -0500

    Parte java script

---
+++ SpringBoot_REST_API_Blueprints_Part2/src/main/resources/static/js/app.js
@@ -0,0 +1,33 @@
+var app = (function () {
+    
+    var authname = '';
+    var listBluePrints = [];
+    var data = apimock.getData();
+    
+    var _loadBlueprints = () =>{
+      data.map( (auth) => {
+        auth.map( (bluePrint) =>{
+          const object = {};
+          object.name = bluePrint[Object.keys(bluePrint)[2]];
+          object.nPoints = bluePrint[Object.keys(bluePrint)[1]].length;
+          listBluePrints.push(object);
+        })
+      })
+    }
+
+    var publicMethod = function () {
+      _loadBlueprints();
+      
+      
+    };
+    var setAuthorName = (name) =>{
+      ('#auth').val(name);
+    }
+  
+    return {
+      publicMethod: publicMethod,
+      setAuthorName : setAuthorName
+
+    };
+  
+  })();
