b7e6fe1bab7e3882e6d80f918d30609bd3fb812b
Juan Espinosa <juan.espinosa-t@mail.escuelaing.edu.co>
2022-03-07 19:50:49 -0500

    logica de front-end casi completa

--- SpringBoot_REST_API_Blueprints_Part2/src/main/resources/static/js/app.js
+++ SpringBoot_REST_API_Blueprints_Part2/src/main/resources/static/js/app.js
@@ -1,34 +1,54 @@
-var app = (function () {
-    
-    var authname = '';
-    var listBluePrints = [];
-    var data = apimock.getData();
-    
-    var _loadBlueprints = () =>{
-      data.map( (auth) => {
-        auth.map( (bluePrint) =>{
-          const object = {};
-          object.name = bluePrint[Object.keys(bluePrint)[2]];
-          object.nPoints = bluePrint[Object.keys(bluePrint)[1]].length;
-          listBluePrints.push(object);
-        })
-      })
-    }
+const app = (function () {
 
-    var publicMethod = function () {
-      _loadBlueprints();
+  var _authname = '';
+  var listBluePrints = [];
+  var points = 0;
+
+
+  var _loadBlueprints = (data) => {
+    data.map((bluePrint) => {
+      const object = {};
+      object.name = bluePrint[Object.keys(bluePrint)[2]];
+      object.nPoints = bluePrint[Object.keys(bluePrint)[1]].length;
+      listBluePrints.push(object);
+    })
+
+  }
+
+  var _setAuthorName = (name) => {
+    let text = name + "'s Blueprints";
+    $('#author').html(text);
+  }
+
+  var setBlueprintsList = function (auth) {
+    $('#table tbody').empty();
+    _authname = auth;
+    if (_authname == '') {
+      alert('Please enter a name');
+    }
+    else {
+      _setAuthorName(_authname);
+      apimock.getBlueprintsByAuthor(_authname, _loadBlueprints);
       console.log(listBluePrints);
-      
-      
-    };
-    var setAuthorName = (name) =>{
-      ('#auth').val(name);
+      listBluePrints.map(obj => {
+        $('#table > tbody:last')
+          .append($(`
+              <tr>
+                  <td>${obj.name}</td>
+                  <td>${obj.nPoints}</td>
+              </tr>`));
+        points += obj.nPoints;    
+        $("#totalPoints").html("Total user Points: " + points);
+      });
     }
-  
-    return {
-      publicMethod: publicMethod,
-      setAuthorName : setAuthorName
 
-    };
+  };
   
-  })();
+
+  return {
+    setBlueprintsList: setBlueprintsList,
+    
+
+  };
+
+})();
