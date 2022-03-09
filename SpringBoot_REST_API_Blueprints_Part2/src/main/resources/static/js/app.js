8f62a4007be3b2e752ecfc56e51a38b1d4a74cac
Juan Espinosa <juan.espinosa-t@mail.escuelaing.edu.co>
2022-03-08 22:05:48 -0500

    avance canvas

--- SpringBoot_REST_API_Blueprints_Part2/src/main/resources/static/js/app.js
+++ SpringBoot_REST_API_Blueprints_Part2/src/main/resources/static/js/app.js
@@ -36,18 +36,38 @@ const app = (function () {
               <tr>
                   <td>${obj.name}</td>
                   <td>${obj.nPoints}</td>
-              </tr>`));
-        points += obj.nPoints;    
+                  <td><button class="btn btn-primary">Open</button></td>
+              </tr>`).on("click", "button", () => drawCanvas(_authname, obj.name)));
+        points += obj.nPoints;
         $("#totalPoints").html("Total user Points: " + points);
       });
     }
 
   };
-  
+
+  const drawCanvas = (author, bname) => {
+        
+    apimock.getBlueprintsByNameAndAuthor(author, bname, (data) => {   
+      const points = data.points;
+      let canvas = $('#canvas')[0];
+      if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.moveTo(points[0].x, points[0].y);
+        console.log(points[0].x, points[0].y);
+        for (var i = 1; i < points.length; i++) {
+          console.log(points[i].x, points[i].y);
+          ctx.lineTo(points[i].x, points[i].y);
+        }
+        ctx.stroke();
+      }
+    })
+  }
+
 
   return {
     setBlueprintsList: setBlueprintsList,
-    
+
 
   };
 
